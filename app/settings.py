3eeb936783945507a36747ca163f54805f91bc10
bubaley <bubaley.fu@gmail.com>
2024-10-25 14:43:16 +0200

    init

--- app/settings.py
+++ app/settings.py
@@ -5,6 +5,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    dumps_folder: str
     redis_dsn: RedisDsn = Field(default='redis://localhost:6379/1')
     debug: bool = Field(True)
 
